ad2404b210bb417a8a035c351ebed2787f9c92f5
Jack <dachtler@live.co.uk>
2020-08-10 21:10:10 +0100

    sudoku grid rendered succesfully

--- scripts/sudoku-grid.js
+++ scripts/sudoku-grid.js
@@ -22,20 +22,25 @@ class SudokuGrid {
       const row = document.createElement("div");
       row.classList.add("row");
 
-      
-
       for (let x = 0; x < 9; x++) {
         const box = document.createElement("div");
         box.classList.add("box", "box-hover");
 
         if (x % 3 === 0) {
-          box.classList.add("smallGridx");
+          box.classList.add("bl-5");
+        }
+
+        if (x === 8) {
+          box.classList.add("br-5");
         }
 
         if (y % 3 === 0) {
-          box.classList.add("smallGridy");
+          box.classList.add("bt-5");
         }
 
+        if (y === 8) {
+          box.classList.add("bb-5");
+        }
 
         if (x == this.state.selected.x && y == this.state.selected.y) {
           box.classList.add("selected");
